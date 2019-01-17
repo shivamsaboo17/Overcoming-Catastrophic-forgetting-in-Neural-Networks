936718c7898d46e78dcf844d5b76556b79eda3d6
shivamsaboo17 <shivamsaboo3007@gmail.com>
2019-01-17 10:15:34 +0530

    Added initial files

---
+++ elastic_weight_consolidation.py
@@ -0,0 +1,63 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch import autograd
+import numpy as np
+
+
+class ElasticWeightConsolidation:
+
+    def __init__(self, model, crit, lr=0.001, weight=0.1):
+        self.model = model
+        self.weight = weight
+        self.crit = crit
+        self.optimizer = optim.Adam(self.model.parameters(), lr)
+
+    def _update_mean_params(self):
+        for param_name, param in self.model.named_parameters():
+            _buff_param_name = param_name.replace('.', '__')
+            self.model.register_buffer(_buff_param_name+'_estimated_mean', param.data.clone())
+
+    def _update_fisher_params(self, current_ds, batch_size, num_batch):
+        dl = DataLoader(ds, batch_size, shuffle=True)
+        log_liklihoods = []
+        for i, (input, target) in enumerate(dl):
+            if i > num_batch:
+                break
+            output = F.log_softmax(self.model(input), dim=1)
+            log_liklihoods.append(output[:, target])
+        log_likelihood = torch.cat(log_liklihoods).mean()
+        grad_log_liklihood = autograd.grad(log_likelihood, self.model.parameters())
+        _buff_param_names = [param[0].replace('.', '__') for param in self.model.named_parameters()]
+        for _buff_param_name, param in zip(_buff_param_names, grad_log_liklihood):
+            self.model.register_buffer(_buff_param_name+'_estimated_fisher', param.data.clone() ** 2)
+
+    def register_ewc_params(self, dataset, batch_size, num_batches):
+        self._update_fisher_params(dataset, batch_size, num_batches)
+        self._update_mean_params()
+
+    def _compute_consolidation_loss(self, weight):
+        try:
+            losses = []
+            for param_name, param in self.model.named_parameters():
+                _buff_param_name = param_name.replace('.', '__')
+                estimated_mean = getattr(self.model, '{}_estimated_mean'.format(_buff_param_name))
+                estimated_fisher = getattr(self.model, '{}_estimated_fisher'.format(_buff_param_name))
+                losses.append((estimated_fisher * (param - estimated_mean) ** 2).sum())
+            return (weight / 2) * sum(losses)
+        except AttributeError:
+            return 0
+
+    def forward_backward_update(self, input, target):
+        output = self.model(input)
+        loss = self._compute_consolidation_loss(self.weight) + self.crit(output, target)
+        self.optimizer.zero_grad()
+        loss.backward()
+        self.optimizer.step()
+
+    def save(self):
+        torch.save(self.model)
+        
+    def load(self, filename):
+        self.model = torch.load(filename)
